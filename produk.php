f15af61312a9047ac114ef7243244af77e17923c
PandaSC <43714859+riyansc@users.noreply.github.com>
2022-05-08 22:13:38 +0700

    Update Materi Property dan Method
    Menambahkan Property dan Method pada php, dengan membuat sebuah property di dalam class Produk, yaitu public nama,penulis,penerbit dan harga, kemudian menambahkan method function getLabel, untuk di panggil secara berulang dengan tipe public

--- produk.php
+++ produk.php
@@ -1,15 +1,50 @@
 <?php 
 
-//jualan produk
+// jualan produk
 // komik
 // game
 
 class Produk {
 
- 
+	public $judul = "judul", $penulis ="penulis", $penerbit = "penerbit", $harga= 0;
+ 	// Method adalah function yang ada di dalam kelas
+
+    public function getLabel() {
+        // Ketika akan mengambil data yang ada di dalam property lain maka gunakan $this untuk memanggilnya
+        return "$this->penulis, $this->penerbit";
+
+    }
  }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
 
+// Membuat Object dan menimpa data yang ada di dalam property public
+// Contoh
+// $produk1 = new Produk();
+// $produk1->judul = 'Naruto';
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// var_dump($produk2);
+
+ $produk3 = new Produk();
+ $produk3->judul="Naruto";
+ $produk3->penulis="masashi kisimoto";
+ $produk3->penerbit="Shonen Jump";
+ $produk3->harga="30000";
+
+$produk4 = new Produk();
+ $produk4->judul="Uncharted";
+ $produk4->penulis="Neil Duchman";
+ $produk4->penerbit="Sony Computer";
+ $produk4->harga="2500000";
+
+
+ // echo "Komik : $produk3->judul, $produk3->penulis, $produk3->penerbit, $produk3->harga";
+ // echo "<br>";
+// echo $produk3->getLabel();
+
+ echo "Komik : " . $produk3->getLabel();
+ echo "<br>";
+ echo "Game : " . $produk4->getLabel();
  ?>
