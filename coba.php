0e1d2742ba305f2a7ba5bcbba7b8b445be223395
PandaSC <43714859+riyansc@users.noreply.github.com>
2022-05-08 21:43:06 +0700

    Belajar Class dan Object
    Mempelajari apa itu class dan object dalam php

---
+++ coba.php
@@ -0,0 +1,14 @@
+<?php 
+//Contoh penulisan class
+class Coba {
+
+
+}
+
+// contoh object instance
+// menggunakan variable
+$a = new Coba();
+
+var_dump($a);
+
+ ?>
---
+++ produk.php
@@ -0,0 +1,15 @@
+<?php 
+
+//jualan produk
+// komik
+// game
+
+class Produk {
+
+ 
+ }
+
+$produk1 = new Produk();
+$produk2 = new Produk();
+
+ ?>
