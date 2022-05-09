e0c490ac161cad997b2a24b5c85962a98641cf21
PandaSC <43714859+riyansc@users.noreply.github.com>
2022-05-09 23:27:33 +0700

    Constant

---
+++ constant.php
@@ -0,0 +1,24 @@
+<?php 
+
+// Constant digunakan untuk menyimpan nilai, tetapi berbeda dengan variable
+
+// define('NAMA', 'Riyan Saputra');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 32;
+// echo UMUR;
+
+
+// class Coba {
+
+// 	const Nama = "Riyan Saputra";
+
+//  }
+
+//  echo  Coba::Nama;
+
+
+// Line untuk melihat baris penulisan code line
+echo __LINE__;
