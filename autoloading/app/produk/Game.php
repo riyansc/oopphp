53ff7e18a10d287f1beabd62e7db0e15a0b49d5a
PandaSC <43714859+riyansc@users.noreply.github.com>
2022-05-10 14:07:58 +0700

    Autoloading
    Memanggil seluruh kelas yang ada di semua file produk dengan ekstensi php, autoloading hanya menggunakan 1 perintah berbeda dengan require yang harus memanggil satu persatu

---
+++ autoloading/app/produk/Game.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Game extends Produk implements InfoProduk {
+    public $waktuMain;
+    public function __construct ($judul, $penulis,$penerbit, $harga, $waktuMain){
+
+        parent::__construct($judul, $penulis,$penerbit, $harga, $waktuMain);
+        $this->waktuMain = $waktuMain;
+
+    }
+    public function getInfo() {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        
+        return $str;
+    }
+
+    public function getInfoProduk(){
+        $str = "Game : " . $this->getInfo() . " ~ {$this->waktuMain} Jam";
+        return $str;
+    }
+ }
---
+++ autoloading/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+require_once 'app/init.php';
+$produk1 = new Komik("Naruto", "Masashi Kisimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 20000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+
+echo $cetakProduk->cetak();
